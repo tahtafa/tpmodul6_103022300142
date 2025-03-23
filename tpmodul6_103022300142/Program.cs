cd466be3b4435eceb8877732522c378cc8818f61
tahtafa <tahtaccnt@gmail.com>
2025-03-24 04:46:08 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300142/Program.cs
+++ tpmodul6_103022300142/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//first
+using tpmodul6_103022300142;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - M. Tahtafaiz Madani");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_103022300142/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300142
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+
+        }
+    }
+}
