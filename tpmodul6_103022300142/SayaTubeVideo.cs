67b3ee60559a7fc8bb1f5228bb36dc546d3418d9
tahtafa <tahtaccnt@gmail.com>
2025-03-24 05:06:31 +0700

    implementasi design by contract

--- tpmodul6_103022300142/SayaTubeVideo.cs
+++ tpmodul6_103022300142/SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace tpmodul6_103022300142
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(!string.IsNullOrEmpty(title) && title.Length <= 100, "Title must not be null and should have a maximum length of 100 characters.");
+
             Random rand = new Random();
             this.id = rand.Next(10000, 99999);
             this.title = title;
@@ -22,7 +25,19 @@ namespace tpmodul6_103022300142
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count > 0 && count <= 10000000, "Play count increment should be between 1 and 10,000,000.");
+
+            checked
+            {
+                try
+                {
+                    this.playCount += count;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Error: Play count overflow!");
+                }
+            }
         }
 
         public void PrintVideoDetails()
@@ -30,7 +45,6 @@ namespace tpmodul6_103022300142
             Console.WriteLine("Video ID: " + id);
             Console.WriteLine("Title: " + title);
             Console.WriteLine("Play Count: " + playCount);
-
         }
     }
 }
